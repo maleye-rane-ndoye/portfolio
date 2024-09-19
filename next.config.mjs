29409a2729594f00e05040dccbf1ac75db8025c8
maleye rane <maleyeranendoye@Mydebian.lan>
2024-09-19 13:02:23 +0200

    Deployment on github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    assetPrefix: '/portfolio/', 
+    basePath: '/portfolio', 
+  };
+  
+  export default nextConfig;
+  
