b2c94bbb9ce4b5700a5034f3887ebdf36410f1d2
maleye rane <maleyeranendoye@Mydebian.lan>
2024-09-19 09:57:53 +0200

    Image add

---
+++ components/Hero.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/textGenerateEffect'
+import { FaLocationArrow } from "react-icons/fa";
+
+
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+          <div className='relative'>
+                <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
+                <Spotlight className='-top-10 -left-full h-[80vh] w-[50vh]' fill='purple'/>
+                <Spotlight className='-top-28 -left-80 h-[80vh] w-[50vh] ' fill='blue'/>
+          </div>
+          <div className="h-full w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center relative overflow-hidden">
+            <img src="/b11.jpeg" alt="" className='object-cover w-full h-full mask-image-[linear-gradient(to_top, transparent, black, transparent)]' />
+
+  
+              <div className='absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col text-center items-center justify-center'>
+                <h2 className='uppercase tracking-widest text-xs md:text-xl text-center text-blue-100 '>
+                Transforming the Web with a Blend of Modern <p className='text-purple font-bold'>Tech Stacks</p> 
+                </h2>
+
+                <TextGenerateEffect
+                  className='sm:text-sm'
+                  words='Crafting Innovative Solutions for Modern Web Experiences'
+                />
+                <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl whitespace-nowrap '>
+                  Hi I&apos;m Maleye, a Full Stack Developer based in France
+                </p>
+                <a href="#about">
+                  <MagicButton
+                    title='show my work'
+                    icon={<FaLocationArrow />}
+                    position='right'
+                  />
+                </a>
+              </div>
+          </div>
+
+    </div>
+  )
+}
+
+export default Hero
+
---
+++ data/index.ts
@@ -0,0 +1,279 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+
+  export const techSkills = [
+    { name: "React.js", logo: "/re.svg" },
+    { name: "Next.js", logo: "/next.svg" },
+    { name: "php", logo: "/php.svg" },
+    { name: "JavaScript", logo: "/js.svg" },
+    { name: "tailwindCSS", logo: "/tail.svg" },
+    { name: "MySQL", logo: "#contact" },
+    { name: "TypeScript", logo: "ts.svg" },
+
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Finances-Flow",
+      des: "Empowers users to effortlessly tracke, manage, and visualize their financial transactions in real-time, promoting smarter budgeting and informed spending decisions..js.",
+      img: "/finance.jpeg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/maleye-rane-ndoye/finances-flow.git",
+    },
+    {
+      id: 2,
+      title: "My little MVC",
+      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/php.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/js.svg"],
+      link: "https://github.com/maleye-rane-ndoye/my-little-mvc.git",
+    },
+    {
+      id: 3,
+      title: "Calclator9",
+      des: "Calculatore9 is a virtual calculator coded in React that offers an modern interface for arithmetic operations.",
+      img: "/Calculator9.jpeg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/php.svg", "/next.svg"],
+      link: "https://github.com/maleye-rane-ndoye/calculator9000.git",
+    },
+    {
+      id: 4,
+      title: "Symphony Shop",
+      des: "A modern online store developed using PHP, Symfony, and Composer, offering a seamless shopping experience.",
+      img: "/shop.jpg",
+      iconLists: ["/symfony.svg", "/php.svg", "/tail.svg", "/composer.svg", "/js.svg"],
+      link: "https://github.com/maleye-rane-ndoye/symfony-shop.git",
+    },
+
+    {
+      id: 5,
+      title: "Motus Game",
+      des: "Motus is a word-guessing game where players attempt to deduce a hidden word by making educated guesses and receiving feedback on letter accuracy and placement.",
+      img: "/motus.jpeg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/maleye-rane-ndoye/motus.git",
+    },
+
+    {
+      id: 6,
+      title: "Livre D'or",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/maleye-rane-ndoye/livre-or.git",
+    },
+
+    {
+      id: 7,
+      title: "Clean Express",
+      des: "Clean Express is an app that swiftly connects clients with professional cleaners for immediate cleaning tasks in homes, hotels, and buildings, offering efficient service with real-time notifications.",
+      img: "/clean.jpeg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/maleye-rane-ndoye/Clean-Express.git",
+    },
+
+    {
+      id: 8,
+      title: "Blood rescue",
+      des: "Blood Rescue is a life-saving mobile app that connects blood donors with those in urgent need, offering easy access to donation centers, emergency alerts, and blood requests.",
+      img: "/Blood.jpeg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/maleye-rane-ndoye/blood-rescue.git",
+    },
+    {
+      id: 8,
+      title: "Blue Wave",
+      des: "Blood Rescue is a life-saving mobile app that connects blood donors with those in urgent need, offering easy access to donation centers, emergency alerts, and blood requests.",
+      img: "/BlueWave.jpeg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/maleye-rane-ndoye/bluewave.git",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Working with Maleye has been an incredibly enriching experience. His ability to grasp complex web development concepts and apply them effectively is impressive. His commitment to excellence and eagerness to learn make him an outstanding web developer.",
+      name: "Nicola Revel",
+      title: "Web Development Instructor",
+      profileImage: "/nico.webp", // Replace with the actual path to the image
+    },
+    {
+      quote:
+        "Maleye developed an online store app for me, and the result exceeded all my expectations. He understood my specific needs and translated them into a functional and aesthetically pleasing solution. I highly recommend his services to anyone looking for a skilled and creative developer.",
+      name: "Djamilatou Dollo",
+      title: "Client and Online Store Owner",
+      profileImage: "/djami.enc", // Replace with the actual path to the image
+    },
+    {
+      quote:
+        "As a colleague, I have closely observed Maleye's technical skills. He is not only a talented developer but also an excellent collaborator, always ready to help and share his knowledge. His ability to solve complex problems and work as a team is remarkable.",
+      name: "Abraham Ukachi",
+      title: "Developer",
+      profileImage: "/abra.enc", // Replace with the actual path to the image
+    },
+    {
+      quote:
+        "Maleye demonstrated great professionalism throughout our collaboration. As a mentor, I particularly appreciated his ability to adapt and thrive in a dynamic environment. His proactive approach and passion for development are valuable assets for any organization.",
+      name: "Ekaterina",
+      title: "IT Manager at UNAPEI",
+      profileImage: "/katia.enc", // Replace with the actual path to the image
+    },
+  ];
+  
+  
+  
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "La Plateforme",
+      img: "/plateforme.png",
+      nameImg: "/laplateforme.png",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
+
+
+  // const profile = [
+
+  //   
+  // ]
